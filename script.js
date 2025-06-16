05ae33744932bff2ececf25e092bcbb295316ee7
RupeshKumar4511 <kumarrn4511@gmail.com>
2025-06-16 22:45:31 +0530

    first version updated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch('https://restcountries.com/v3.1/all').then((res) => res.json()
     .then(data => data.forEach((country) => {
 
         countryCards(country);
-        console.log(country)
+        
     })));
 
 
